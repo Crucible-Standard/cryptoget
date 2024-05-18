1f639be3957079adb365c6977307b52981e85b6a
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2024-05-18 14:08:16 -0400

    Update slack.ts

--- src/controllers/slack.ts
+++ src/controllers/slack.ts
@@ -16,28 +16,24 @@ class SlackController extends DefaultController {
     request: express.Request,
     response: express.Response
   ) => {
-    if (request.query.token || request.body.text) {
-      const token = `${request.query.token}` || `${request.body.text}`;
-      // check to see if token is at least 3 characters
-      if (token.length > 2) {
-        const data = await getSingle(token);
-
-        response.status(200).send({
-          response_type: "in_channel",
-          text: `${data.data.message}`,
-        });
-      } else {
-        response.status(400).send({
-          data: {
-            message:
-              "Please use the endpoint with a get param of 'token'. example https://cryptoget.herokuapp.com/?token=eth",
-          },
-          meta: {
-            status: 400,
-          },
-        });
-      }
+    if (!request.query.token && !request.body.text) {
+      response.status(400).send({
+        data: {
+          message:
+            "Please use the endpoint with a get param of 'token'. example https://cryptoget.herokuapp.com/?token=eth",
+        },
+        meta: {
+          status: 400,
+        },
+      });
     }
+    const token = `${request.query.token}` || `${request.body.text}`;
+    const data = await getSingle(token);
+
+    response.status(200).send({
+      response_type: "in_channel",
+      text: `${data.data.message}`,
+    });
   };
 }
 
